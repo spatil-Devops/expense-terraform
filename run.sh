c36f8ddb4892c45a5c9a238c5f7be79fc0f11e46
Patil <shubham.patil5@dell.com>
2025-06-12 16:17:24 +0530

    action

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 terraform init -backend-config=env-${env}/state.tfvars
-terraform apply -var-file=env-${env}/main.tfvars -auto-approve -var vault_token=${vault_token} -var SSH_PASSWORD=${SSH_PASSWORD}
+terraform ${action} -var-file=env-${env}/main.tfvars -auto-approve -var vault_token=${vault_token} -var SSH_PASSWORD=${SSH_PASSWORD}
